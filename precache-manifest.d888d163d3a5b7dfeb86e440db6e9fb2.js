7c3abbf835b2bc73bb1968703ef58d3080afcbe0
Kostia Drobotovych <k.drobotovych@gmail.com>
2019-06-05 14:29:08 +0300

    Updates

---
+++ precache-manifest.d888d163d3a5b7dfeb86e440db6e9fb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "160930f891feab163951f677783247a5",
+    "url": "/movies-app/static/media/whiteBlock.160930f8.jpg"
+  },
+  {
+    "revision": "b5cb075a87b20c3a79cec8af589eade4",
+    "url": "/movies-app/static/media/logo.b5cb075a.svg"
+  },
+  {
+    "revision": "f58da00be1d28636e05a",
+    "url": "/movies-app/static/js/runtime~main.41022d0a.js"
+  },
+  {
+    "revision": "65ca0345f486c2b3d3cd",
+    "url": "/movies-app/static/js/main.d3b8a475.chunk.js"
+  },
+  {
+    "revision": "4a224f0258d9b6553119",
+    "url": "/movies-app/static/js/2.eb87b745.chunk.js"
+  },
+  {
+    "revision": "65ca0345f486c2b3d3cd",
+    "url": "/movies-app/static/css/main.69e8c62b.chunk.css"
+  },
+  {
+    "revision": "4a224f0258d9b6553119",
+    "url": "/movies-app/static/css/2.92fcd2e9.chunk.css"
+  },
+  {
+    "revision": "4ed5af056d9f292dd6abf76a25caf44e",
+    "url": "/movies-app/index.html"
+  }
+];
